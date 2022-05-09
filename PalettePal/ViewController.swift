5d97e34f04e9502f057faaa44d9ee871638d0a88
Lele Luu <leleluu@gmail.com>
2022-05-09 09:37:48 +0200

    Set up project

--- PalettePal/ViewController.swift
+++ PalettePal/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  PalettePal
-//
-//  Created by lele luu on 09.05.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
     }
-
-
 }
 
