f458df9d13f74fb7221aec6a64afe33c3a6b8ceb
Lele Luu <leleluu@gmail.com>
2022-05-09 18:58:51 +0200

    Add generate random palette button

--- PalettePal/RandomPaletteViewController.swift
+++ PalettePal/RandomPaletteViewController.swift
@@ -2,9 +2,39 @@ import UIKit
 
 class RandomPaletteViewController: UIViewController {
 
+    // MARK: - Private Properties
+    
+    private lazy var generateRandomPaletteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("random color palette button", for: .normal)
+        button.backgroundColor = UIColor.systemYellow
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapGenerateRandomPaletteButton), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
+        setupViews()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupViews() {
+        view.addSubview(generateRandomPaletteButton)
+        
+        NSLayoutConstraint.activate([
+            generateRandomPaletteButton.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+            generateRandomPaletteButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            generateRandomPaletteButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    @objc private func didTapGenerateRandomPaletteButton() {
+        
     }
 }
 
