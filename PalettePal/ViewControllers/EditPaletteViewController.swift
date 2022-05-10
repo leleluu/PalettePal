12b38623262f920c5963fb1d9c9d0d0a976a0ee3
Lele Luu <leleluu@gmail.com>
2022-05-10 09:54:14 +0200

    Enable dismissal of edit palette screen

--- PalettePal/ViewControllers/EditPaletteViewController.swift
+++ PalettePal/ViewControllers/EditPaletteViewController.swift
@@ -7,6 +7,14 @@ class EditPaletteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+    }
+    
+    // MARK: - Private Methods
+    
+    @objc private func didTapCancel() {
+        dismiss(animated: true)
     }
 }
 
