c405223503991bcbb477fd38d40b52f66491b6bf
Lele Luu <leleluu@gmail.com>
2022-05-12 13:03:40 +0200

    Show saved palettes on palette screen

--- PalettePal/ViewControllers/PalettesViewController.swift
+++ PalettePal/ViewControllers/PalettesViewController.swift
@@ -2,10 +2,64 @@ import UIKit
 
 class PalettesViewController: UIViewController {
     
+    // MARK: - Private Properties
+
+    private var layout = UICollectionViewFlowLayout()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.register(ColorCell.self, forCellWithReuseIdentifier: ColorCell.id)
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        setupViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        collectionView.reloadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let width = collectionView.frame.width
+        layout.itemSize = CGSize(width: width, height: 100)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupViews() {
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension PalettesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Palettes.all.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ColorCell.id, for: indexPath) as! ColorCell
+        let color = Palettes.all[indexPath.row].colors[0]
+        cell.backgroundColor = color
+        cell.rgbLabel.text = color.rgbString
+        cell.hexLabel.text = color.hexString
+        return cell
     }
 }
