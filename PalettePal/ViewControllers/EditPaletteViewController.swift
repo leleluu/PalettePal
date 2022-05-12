8ddb28e68f63142d4e2dc91e1bf731e7b7f4a772
Lele Luu <leleluu@gmail.com>
2022-05-12 10:13:11 +0200

    Display first color from palette on edit screen

--- PalettePal/ViewControllers/EditPaletteViewController.swift
+++ PalettePal/ViewControllers/EditPaletteViewController.swift
@@ -2,6 +2,17 @@ import UIKit
 
 class EditPaletteViewController: UIViewController {
     
+    // MARK: - Private Properties
+    
+    private var palette: [UIColor]
+    
+    private lazy var swatch: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
     // MARK: - Initialization
     
     init(palette: [UIColor]) {
@@ -17,13 +28,28 @@ class EditPaletteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(didTapSave))
+        
+        setupViews()
     }
     
     // MARK: - Private Methods
+    
+    private func setupViews() {
+        view.addSubview(swatch)
+        swatch.backgroundColor = palette[0]
+
+        
+        NSLayoutConstraint.activate([
+            swatch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swatch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            swatch.heightAnchor.constraint(equalToConstant: 100),
+            swatch.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
     
     @objc private func didTapCancel() {
         dismiss(animated: true)
@@ -31,6 +57,7 @@ class EditPaletteViewController: UIViewController {
     
     @objc private func didTapSave() {
         print("tapped save")
+        print(palette)
         dismiss(animated: true)
     }
 }
