3ff132c92ab1d768e7aed2d5fc3e0b8d67c6baf1
Lele Luu <leleluu@gmail.com>
2022-05-15 21:10:56 +0200

    Add search functionality for palettes

--- PalettePal/ViewControllers/PalettesViewController.swift
+++ PalettePal/ViewControllers/PalettesViewController.swift
@@ -1,9 +1,24 @@
 import UIKit
 
-class PalettesViewController: UIViewController {
+class PalettesViewController: UIViewController, UISearchControllerDelegate {
     
     // MARK: - Private Properties
 
+    private var filteredPalettes: [Palette] = []
+    private var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    private var isFiltering: Bool {
+      return searchController.isActive && !isSearchBarEmpty
+    }
+
+    private var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "Search for a palette"
+        return searchController
+       }()
+       
     private var layout = UICollectionViewFlowLayout()
     
     private lazy var collectionView: UICollectionView = {
@@ -24,17 +39,26 @@ class PalettesViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "My Palettes"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationController?.navigationItem.largeTitleDisplayMode = .always
-        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+        
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        navigationItem.searchController = searchController
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+        
         setupViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         collectionView.reloadData()
     }
-    
+
     override func viewDidLayoutSubviews() {
+        if layout.itemSize.width == collectionView.frame.width {
+            return
+        }
         let width = collectionView.frame.width
         layout.itemSize = CGSize(width: width, height: 100)
     }
@@ -51,18 +75,37 @@ class PalettesViewController: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    private func filterContentForSearchText(searchText: String) {
+        filteredPalettes = Palettes.all.filter({ palette in
+            palette.name.lowercased().contains(searchText.lowercased())
+        })
+        collectionView.reloadData()
+        
+    }
 }
 
 // MARK: - UICollectionViewDataSource
 
 extension PalettesViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if isFiltering {
+          return filteredPalettes.count
+        }
         return Palettes.all.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PaletteCardCell.id, for: indexPath) as! PaletteCardCell
-        let palette = Palettes.all[indexPath.row]
+        
+        let palette: Palette
+        if isFiltering {
+          palette = filteredPalettes[indexPath.row]
+        } else {
+          palette = Palettes.all[indexPath.row]
+        }
+        
         cell.configure(palette: palette.colors, name: palette.name)
         return cell
     }
@@ -72,8 +115,26 @@ extension PalettesViewController: UICollectionViewDataSource {
 
 extension PalettesViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedPalette = Palettes.all[indexPath.row]
+        
+        let selectedPalette: Palette
+        
+        if isFiltering {
+            selectedPalette = filteredPalettes[indexPath.row]
+        } else {
+            selectedPalette = Palettes.all[indexPath.row]
+        }
+        
         let paletteViewController = PaletteViewController(palette: selectedPalette.colors, name: selectedPalette.name)
         navigationController?.pushViewController(paletteViewController, animated: true)
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension PalettesViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchText: searchBar.text!)
     }
 }
