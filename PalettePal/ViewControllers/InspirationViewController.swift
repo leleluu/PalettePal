4aa026f00495c07b59953135ab73f8b8567a8bb2
Lele Luu <leleluu@gmail.com>
2022-05-29 16:48:10 +0200

    Set up collection view with compositional layout

--- PalettePal/ViewControllers/InspirationViewController.swift
+++ PalettePal/ViewControllers/InspirationViewController.swift
@@ -3,6 +3,15 @@ import UIKit
 class InspirationViewController: UIViewController {
 
     // MARK: - Private Properties
+        
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .systemYellow
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     
     // MARK: - Lifecycle
     
@@ -15,8 +24,72 @@ class InspirationViewController: UIViewController {
     // MARK: - Private Methods
     
     private func setupViews() {
-//        view.addSubview(collectionView)
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(2/3), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+
+        let inset: CGFloat = 5
+        item.contentInsets = NSDirectionalEdgeInsets(
+          top: inset,
+          leading: inset,
+          bottom: inset,
+          trailing: inset
+        )
+        
+        // Group
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1/3))
+
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
 }
     
+// MARK: - UICollectionViewDataSource
+
+extension InspirationViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+         
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 1 {
+            return 5
+        } else {
+            return 10
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemIndigo
+        } else {
+            cell.backgroundColor = .systemMint
+        }
+        return cell
+    }
+}
