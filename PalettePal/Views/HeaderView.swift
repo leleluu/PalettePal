835414e1cbb484e12fd9bab5d418cbbea06c04b1
Lele Luu <leleluu@gmail.com>
2022-05-31 17:36:57 +0200

    Add separator

--- PalettePal/Views/HeaderView.swift
+++ PalettePal/Views/HeaderView.swift
@@ -6,6 +6,13 @@ class HeaderView: UICollectionReusableView {
     
     static let id = "Header"
     
+    private let separator: UIView = {
+        let separator = UIView(frame: .zero)
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        separator.backgroundColor = .quaternaryLabel
+        return separator
+    }()
+    
     var titleLabel: UILabel = {
         let label: UILabel = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 22)
@@ -18,8 +25,13 @@ class HeaderView: UICollectionReusableView {
         super.init(frame: frame)
         
         addSubview(titleLabel)
+        addSubview(separator)
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
+            separator.topAnchor.constraint(equalTo: topAnchor),
+            separator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separator.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: 4),
             titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
