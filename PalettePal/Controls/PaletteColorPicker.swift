c6cf29e3f43ce6c846250d34f09251ce581d99b9
Lele Luu <leleluu@gmail.com>
2022-06-06 15:41:44 +0200

    Indicate selected color

--- PalettePal/Controls/PaletteColorPicker.swift
+++ PalettePal/Controls/PaletteColorPicker.swift
@@ -8,6 +8,7 @@ class PaletteColorPicker: UIControl {
     
     // MARK: - Private Properties
     
+    private let selectedBorderWidth: CGFloat = 3
     private let palette: Palette
     
     private lazy var stackView: UIStackView = {
@@ -26,21 +27,25 @@ class PaletteColorPicker: UIControl {
         self.selectedColor = palette.colors[0]
 
         super.init(frame: .zero)
-        
+
         for color in palette.colors {
             let button = UIButton()
             button.layer.cornerRadius = 10
-            
             button.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
             button.layer.shadowOpacity = 0.8
             button.layer.shadowRadius = 1.0
             button.layer.shadowColor = UIColor.lightGray.cgColor
-            
             button.addTarget(self, action: #selector(didTapColor(_:)), for: .touchUpInside)
             button.translatesAutoresizingMaskIntoConstraints = false
             button.backgroundColor = color
+            button.layer.borderColor = UIColor.systemOrange.cgColor
+            button.layer.borderWidth = 0
             stackView.addArrangedSubview(button)
         }
+
+        // Mark first button as selected
+        let button = stackView.arrangedSubviews[0]
+        button.layer.borderWidth = selectedBorderWidth
         
         setupSubviews()
     }
@@ -64,6 +69,15 @@ class PaletteColorPicker: UIControl {
     
     @objc func didTapColor(_ sender: UIButton) {
         selectedColor = sender.backgroundColor!
+        
+        for button in stackView.arrangedSubviews {
+            if button == sender {
+                button.layer.borderWidth = selectedBorderWidth
+            } else {
+                button.layer.borderWidth = 0
+            }
+        }
+        
         sendActions(for: .valueChanged)
     }
     
